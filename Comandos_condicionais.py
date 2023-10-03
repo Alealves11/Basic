de858aaaf48587745f6f7784c6e7bdcbfa21865f
Alexandre Pinheiro <alexandre.pinheiro@amlabs.com.br>
2023-10-03 16:53:23 -0300

    Primeiro commit

---
+++ Comandos_condicionais.py
@@ -0,0 +1,13 @@
+# -*- coding: utf -8 -*- #comentario funcional, pra acentuação
+x=-1
+y=10
+z=19
+soma = x + y + z
+print(soma) #apenas pra mostrar o valor da soma das três variaveis
+if x <=soma:
+	if  x<0:
+		print ("x é um valor negativo e menor ou igual a soma")
+	else:
+		print("x é positivo e menor ou igual soma")
+else:
+	print("x maior ou igual a soma")
---
+++ Comandos_condicionais_looping.py
@@ -0,0 +1,7 @@
+# -*- coding: utf -8 -*- #comentario funcional, pra acentuação
+x = 1
+
+while x < 10:
+	print(x)
+	x= x+1
+
